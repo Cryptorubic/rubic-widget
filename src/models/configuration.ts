4be0630b787050f0dc8d366e11792ae4f742e335
siandrev <andreev.sergey.i@yandex.ru>
2021-10-28 13:50:25 +0300

    Avalanche tokens added

--- src/models/configuration.ts
+++ src/models/configuration.ts
@@ -1,7 +1,7 @@
 import {BLOCKCHAIN_NAME} from "./BLOCKCHAIN_NAME";
 import {IframeType} from "./iframe-type";
 
-export type InjectTokensBlockchains = 'eth' | 'bsc' | 'polygon' | 'harmony';
+export type InjectTokensBlockchains = 'eth' | 'bsc' | 'polygon' | 'harmony' | 'avalanche';
 
 export type InjectTokensQuery = Partial<{
     [key in InjectTokensBlockchains as `${key}_tokens`]: string;
